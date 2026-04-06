fdf7c13efea163196d6ef6815443450c6b67f811
Bhupendra1770 <bhupendra360i@gmail.com>
2026-04-06 09:26:28 +0530

    config fixed secret load from .env and update cloude as well

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
-    DATABASE_URL: str = 'postgresql://neondb_owner:npg_A2QvuNs1qDtE@ep-blue-haze-a1u3o3at-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-    SECRET_KEY: str = 'ustybdhbjnbcbhjchbvgvxhjxyuegcyhbcbeycvtvcb'
-    ALGORITHM: str = 'HS256'
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
-    FRONTEND_URL: str = 'https://hrms-frontend-blue.vercel.app'
+    FRONTEND_URL: str = "https://hrms-frontend-blue.vercel.app"
 
 settings = Settings()
